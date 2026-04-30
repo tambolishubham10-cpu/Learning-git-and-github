e6ab8b70d25ebaee145ed934286d19cb8cf42cbb
SHUBHAM TAMBOLI <tambolishubham10@gmail.com>
2026-04-30 06:28:43 +0530

    learnt to upload a file

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import Buttons from "./Components/Button"
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>Hello, Bootstrap!</h1>
+    <Buttons/>
+    <br />
+    <Buttons/>
+     </>
+  )
+}
+
+export default App
